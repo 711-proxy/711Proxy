dd3de6e02e7a13ed5bedd244f9344b6c2eeff727
711Proxy <gyc7906@gmail.com>
2025-07-07 16:11:28 +0800

    Update request.js

--- request/request.js
+++ request/request.js
@@ -4,7 +4,7 @@
     // user:pass http
     require("request-promise")({
       url: "http://ipinfo.io/json",
-      proxy: "http://user:pass@global.rotgb.711proxy.com:10000",
+      proxy: "http://user:pass@ip:port",
     }).then(
       function (data) {
         console.log(data);
